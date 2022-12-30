f103dca4c172395dbee083e43e01619df26a60e5
JunyaMatsushita <matsuwork6@gmail.com>
2022-12-31 00:48:05 +0900

    flexDirction 確認

--- App.tsx
+++ App.tsx
@@ -1,21 +1,12 @@
-import { StatusBar } from "expo-status-bar"
-import { StyleSheet, View } from "react-native"
-import { HelloWorld } from "./src/components/HelloWorld"
+import NewsList from "./src/pages/NewsList"
+import { NewsItem } from "./src/components/news/NewsItem"
 
+import SamplePage from "./src/pages/SamplePage"
 export default function App() {
   return (
-    <View style={styles.container}>
-      <HelloWorld message="ReactNative!" />
-      <StatusBar style="auto" />
-    </View>
+    // <NewsList>
+    //   <NewsItem />
+    // </NewsList>
+    <SamplePage />
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-})
