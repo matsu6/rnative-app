3a06e11e12b4ddd3df3988ddaa61f420d085ef47
JunyaMatsushita <matsuwork6@gmail.com>
2022-12-31 21:06:38 +0900

    newsapiから一覧取得ページ作成

--- src/types/article.ts
+++ src/types/article.ts
@@ -1,6 +1,19 @@
 export type article = {
+  source: {
+    id: string
+    name: string
+  }
   author: string
   title: string
+  description: string
+  url: string
   urlToImage: string
   publishedAt: string
+  content: string
+}
+
+export type articles = {
+  status: string
+  totalResults: number
+  articles: article[]
 }
